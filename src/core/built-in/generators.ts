0bd694dc2f26b07fdf1b861f664bd5e19a9133d6
Julian Lang <julian.lang@hotmail.de>
2019-11-02 20:09:41 +0100

    tests: add specs for randomIndex and randomItem

--- src/core/built-in/generators.ts
+++ src/core/built-in/generators.ts
@@ -42,7 +42,11 @@ export function randomIndex(values: any[]): number {
  * Gets a random item from a given array. If the array has no items, `null` will be returned.
  * @param values The array from which to get a random item
  */
-export function randomItem(values: any[]): any {
+export function randomItem<T>(values: T[]): T | null {
+  if (values.length === 0) {
+    return null;
+  }
+
   const index = randomIndex(values);
 
   return values[index];
--- src/core/built-in/specs/generators.spec.ts
+++ src/core/built-in/specs/generators.spec.ts
@@ -1,4 +1,4 @@
-import { probability, random, randomInt } from '../generators';
+import { probability, random, randomIndex, randomInt, randomItem } from '../generators';
 
 describe('generators: random(min, max)', () => {
   it('should return a number', () => {
@@ -67,40 +67,102 @@ describe('generators: randomInt(min, max)', () => {
       expect(validResults.includes(value)).toBe(true);
     }
   });
+});
 
-  describe('generators: probability(n)', () => {
-    it('should return a boolean', () => {
-      // arrange
+describe('generators: randomIndex', () => {
+  it('should throw for null and undefined', () => {
+    // arrange
+    const nullInput = null;
+    const undefinedInput = undefined;
+
+    // act
+    // assert
+    expect(() => randomIndex(nullInput as any)).toThrow();
+    expect(() => randomIndex(undefinedInput as any)).toThrow();
+  });
+
+  it('should return 0 for an empty array', () => {
+    // arrange
+    const array: any[] = [];
+
+    // act
+    const index = randomIndex(array);
+
+    // assert
+    expect(index).toBe(0);
+    expect(() => array[index]).not.toThrow();
+  });
+
+  it('should return a valid index for array', () => {
+    // arrange
+    const array = ['a', 'b', 'c'];
+
+    // act
+    const index = randomIndex(array);
+
+    // assert
+    expect(index).toBeLessThan(array.length);
+    expect(index).toBeGreaterThanOrEqual(0);
+  });
+});
+
+describe('generators: randomItem', () => {
+  it('should return null for an empty array', () => {
+    // arrange
+    const array: any[] = [];
+
+    // act
+    const item = randomItem(array);
+
+    // assert
+    expect(item).toBe(null);
+  });
+
+  it('should return an item of a non-empty array', () => {
+    // arrange
+    const item = 1;
+    const array: any[] = [item, item];
+
+    // act
+    const chosenItem = randomItem(array);
+
+    // assert
+    expect(chosenItem).toBe(item);
+  });
+});
+
+describe('generators: probability(n)', () => {
+  it('should return a boolean', () => {
+    // arrange
+    // act
+    const value = probability(0.5);
+
+    // assert
+    expect(typeof value).toEqual('boolean');
+  });
+
+  it('should respect the probability factor n', () => {
+    // arrange
+    const toleratedDeviation = 0.25;
+    const testCycles = 1000;
+    const probFactor = 0.5; // 50% likely to be true
+    const toleratedMin = probFactor - toleratedDeviation;
+    const toleratedMax = probFactor + toleratedDeviation;
+
+    let positiveResults = 0;
+
+    for (let i = 0; i < testCycles; i++) {
       // act
-      const value = probability(0.5);
+      const value = probability(probFactor);
 
-      // assert
-      expect(typeof value).toEqual('boolean');
-    });
-
-    it('should respect the probability factor n', () => {
-      // arrange
-      const toleratedDeviation = 0.25;
-      const testCycles = 1000;
-      const probFactor = 0.5; // 50% likely to be true
-      const toleratedMin = probFactor - toleratedDeviation;
-      const toleratedMax = probFactor + toleratedDeviation;
-
-      let positiveResults = 0;
-
-      for (let i = 0; i < testCycles; i++) {
-        // act
-        const value = probability(probFactor);
-
-        if (value === true) {
-          positiveResults++;
-        }
+      if (value === true) {
+        positiveResults++;
       }
+    }
 
-      // assert
-      const average = positiveResults / testCycles;
-      expect(average).toBeGreaterThan(toleratedMin);
-      expect(average).toBeLessThan(toleratedMax);
-    });
+    // assert
+    const average = positiveResults / testCycles;
+    expect(average).toBeGreaterThan(toleratedMin);
+    expect(average).toBeLessThan(toleratedMax);
   });
 });
