5b1f69c7fc77bbe7f0b36338460f6696aff88f9c
Julian Lang <julian.lang@hotmail.de>
2019-11-19 18:38:18 +0100

    refactor: remove implementation and tests for time

--- src/builders/time.ts
+++ src/builders/time.ts
@@ -1,58 +1,26 @@
-import moment from 'moment';
-import { randomDate } from '../core';
-import { isDefined } from '../util';
-import { DateParserFn, DateTimeOpts } from './types';
+import { Buildable, BuildableSymbol, BuilderFn, createBuilderFn, ProcessorFn } from '../core';
+import { DateTimeOpts } from './types';
 
 /**
  * Generates a random time, while using today's date.
  * @param earliest Time string to be used as earliest allowed time. HH:mm:ss - `13:15:00`
  * @param latest Time string to be used as latest allowed time. HH:mm:ss - `13:15:00`
  */
-export function time(earliest: string, latest: string, opts?: DateTimeOpts): Date {
-  if (!isDefined(opts)) {
-    opts = {};
-  }
+export function time(
+  earliest: string = '00:00:00',
+  latest: string = '23:59:59',
+  opts?: DateTimeOpts,
+  ...processors: ProcessorFn[]
+): Buildable<BuilderFn> {
+  const timeBuilder = createBuilderFn(timeImpl);
 
-  const customParserFns = opts.parserFns || [];
-  const parserFns = [...customParserFns, parseFormat, parseKeywordFuture];
+  return {
+    [BuildableSymbol]: 'template',
+    processors,
+    value: timeBuilder,
+  };
 
-  const start = tryParse(parserFns, earliest, opts.startFormat);
-  const end = tryParse(parserFns, latest, opts.endFormat);
-
-  if (!isDefined(start)) {
-    throw new Error(`faketastic: Could not parse time's "earliest"-parameter: "${earliest}".`);
-  }
-  if (!isDefined(end)) {
-    throw new Error(`faketastic: Could not parse time's "latest"-parameter: "${latest}".`);
-  }
-
-  return randomDate(start, end);
-}
-
-function tryParse(parserFns: DateParserFn[], input: string, format?: string): Date | null {
-  for (const parserFn of parserFns) {
-    const result = parserFn(input, format);
-
-    if (isDefined(result)) {
-      return result;
-    }
-  }
-
-  return null;
-}
-
-function parseFormat(time: string, format?: string): Date | null {
-  try {
-    return moment(time, format).toDate();
-  } catch {
+  function timeImpl() {
     return null;
   }
-}
-function parseKeywordFuture(time: string): Date | null {
-  if (time.toLowerCase() !== 'future') {
-    return null;
-  }
-
-  // todo: fix
-  return new Date();
 }
