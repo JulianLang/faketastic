25fc400584cfdce7e34f23d3695c55dd7b7722cc
Julian Lang <julian.lang@hotmail.de>
2019-11-23 19:14:16 +0100

    docs: improve comment

--- src/core/util/is-buildable-array.ts
+++ src/core/util/is-buildable-array.ts
@@ -2,7 +2,7 @@ import { isDefined } from '../../util';
 import { Buildable } from '../types';
 import { isBuildable } from './is-buildable';
 
-/** Determines whether at least one item of a given array is a Buildable */
+/** Determines whether at least one item of a given array is a `Buildable` */
 export function isBuildableArray<T = any>(value: any): value is (Buildable | T)[] {
   if (!isDefined(value) || !Array.isArray(value)) {
     return false;
