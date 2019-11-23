2977050c5b332a1a793b94842309510b3bfeb0d3
Julian Lang <julian.lang@hotmail.de>
2019-11-23 11:36:56 +0100

    chore: improve playground

--- example.ts
+++ example.ts
@@ -7,9 +7,18 @@
     $ cd path/to/this/repository/root
     $ git update-index --assume-unchanged example.ts
 
-  Now you can change this file without worrying about checking in changes.
+  Now you can edit this file without worrying about checking in these changes.
+  See also: https://stackoverflow.com/a/17410119/3063191 or https://git-scm.com/docs/git-update-index
+
   To execute the code in this file, simply run:
 
     $ npm start
 */
-console.log('faketastic playground works!');
+import { build, template } from './src';
+
+const MyTemplate = template({
+  faketastic: 'playground works!',
+});
+
+const output = build(MyTemplate);
+console.log(output);
