dbea97098a89a352c37aeb6e91d3337c92885cce
Julian Lang <julian.lang@hotmail.de>
2019-11-23 11:29:42 +0100

    chore: make example a playground file

---
+++ example.ts
@@ -0,0 +1,15 @@
+/*
+  Playground. You can try out faketastic here when developing your own functionality.
+  Please do not check in changes of this file.
+
+  To make git ignore this file for changes, enter this command into a terminal:
+
+    $ cd path/to/this/repository/root
+    $ git update-index --assume-unchanged example.ts
+
+  Now you can change this file without worrying about checking in changes.
+  To execute the code in this file, simply run:
+
+    $ npm start
+*/
+console.log('faketastic playground works!');
