c364a3eb6d8b95dce1ba8e69716a83594cf628da
Julian Lang <julian.lang@hotmail.de>
2019-11-23 23:40:03 +0100

    chore: reset playground

--- example.ts
+++ example.ts
@@ -1,19 +1,24 @@
-import { Names } from './resources';
-import { oneOf, someOf } from './src/builders';
-import { build, randomInt, template, use } from './src/core';
-import { quantity } from './src/processors';
+/*
+  Playground. You can try out faketastic here when developing your own functionality.
+  Please do not check in changes of this file.
 
-const Colleague = template({
-  name: oneOf(Names),
-  test: someOf([1, 2, 3, 4]),
-});
+  To make git ignore this file for changes, enter this command into a terminal:
+
+    $ cd path/to/this/repository/root
+    $ git update-index --assume-unchanged example.ts
+
+  Now you can edit this file without worrying about checking in these changes.
+  See also: https://stackoverflow.com/a/17410119/3063191 or https://git-scm.com/docs/git-update-index
+
+  To execute the code in this file, simply run:
+
+    $ npm start
+*/
+import { build, template } from './src';
 
 const MyTemplate = template({
-  participants: use(
-    Colleague,
-    quantity(() => randomInt(2, 3)),
-  ),
+  faketastic: 'playground works!',
 });
 
-const output = build(MyTemplate, () => randomInt(2, 3));
-console.log(JSON.stringify(output, null, 2));
+const output = build(MyTemplate);
+console.log(output);
