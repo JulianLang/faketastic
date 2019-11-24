8517af45281f57e48cdd57413269d77e1b6c0d0f
Julian Lang <julian.lang@hotmail.de>
2019-11-24 05:18:46 +0100

    fix: test case for map processor

--- specs/processors/map.spec.ts
+++ specs/processors/map.spec.ts
@@ -5,7 +5,7 @@ import { includeProcessorFnSpecs } from '../spec-helpers/shared-specs';
 describe('map processor fn', () => {
   it('should call the mapFn with the buildable it is located on', () => {
     // arrange
-    const mapFn = jasmine.createSpy('mapFn', v => v).and.callThrough();
+    const mapFn = jasmine.createSpy('mapFn', (v, _) => v).and.callThrough();
     const value = 42;
     const mapProcessor = map(mapFn);
     const buildable = createBuildable(value, [mapProcessor]);
@@ -15,7 +15,7 @@ describe('map processor fn', () => {
 
     // assert
     expect(mapFn).toHaveBeenCalledTimes(1);
-    expect(mapFn).toHaveBeenCalledWith(buildable);
+    expect(mapFn).toHaveBeenCalledWith(buildable, jasmine.any(Object));
   });
 
   it('should write the mapped value onto the node as value', () => {
