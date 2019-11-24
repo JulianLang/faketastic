238790c66a8e46908a89ffa432daec2d9672ef4a
Julian Lang <julian.lang@hotmail.de>
2019-11-24 01:28:10 +0100

    refactor: improve typings for clone items helper function

--- src/util/clone-items.ts
+++ src/util/clone-items.ts
@@ -1,6 +1,6 @@
 import { clone } from './clone';
 
 /** Makes every (object based) item unique by cloning the value. */
-export function cloneItems(array: any[]): any[] {
+export function cloneItems<T>(array: T[]): T[] {
   return array.map(value => clone(value));
 }
