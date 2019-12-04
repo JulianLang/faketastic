600cb673f6c5a506097cdfb3750269cd30f53307
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-04 16:10:57 +0100

    refactor: extract function to helper function

--- src/core/util/index.ts
+++ src/core/util/index.ts
@@ -3,6 +3,7 @@ export * from './as-buildable';
 export * from './create-buildable';
 export * from './create-builder.fn';
 export * from './create-processor.fn';
+export * from './get-leaf-buildable';
 export * from './is-buildable';
 export * from './is-buildable-array';
 export * from './is-builder.fn';
