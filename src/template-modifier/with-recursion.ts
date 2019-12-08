60435cc1122972b2cff700149f758232d8565394
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-08 11:53:00 +0100

    refactor: wip: recursion fn

--- src/template-modifier/with-recursion.ts
+++ src/template-modifier/with-recursion.ts
@@ -1,5 +1,8 @@
-import { createBuildable, createBuilderFn, ProcessorFn, PureObject } from '../core';
-import { clone } from '../util';
+import { ObjectTreeNode } from 'treelike';
+import { ProcessorOrders } from '../constants';
+import { asBuildable, buildChild, createBuildable, createProcessorFn, ProcessorFn } from '../core';
+import { placeholder } from '../placeholder';
+import { clone, isUndefined } from '../util';
 import { IterationState, RecursionIterator } from './types';
 
 /**
@@ -9,44 +12,34 @@ import { IterationState, RecursionIterator } from './types';
  * @param processors Processors that must include a `quantity(0, x)` or `canBe(any)` processor,
  * being able to end the recursion at some time. Otherwise you'll get a `StackOverflow` exception.
  */
-export function withRecursion<T>(
-  tmpl: PureObject<T>,
+export function recursion(
   property: string,
   endWhen: RecursionIterator,
   ...processors: ProcessorFn[]
 ) {
-  const instruction = iterateNext(endWhen, tmpl);
+  const recursionInit = createProcessorFn(
+    recursionImpl,
+    'initializer',
+    ProcessorOrders.treeStructureChanging,
+  );
 
-  if (instruction.continue) {
-    (tmpl as any)[property] = addRecursiveProperty(endWhen, tmpl);
-  } else {
-    (tmpl as any)[property] = instruction.endWithValue;
-  }
+  return createBuildable(placeholder(`recursion:${property}`), [recursionInit, ...processors]);
 
-  return tmpl;
+  function recursionImpl(node: ObjectTreeNode) {
+    if (isUndefined(node.parent)) {
+      throw new Error(``);
+    }
 
-  /**
-   * Creates a `Buildable` property making its holding template recursive.
-   * @param property The property name being the recursion root
-   * @param tmpl The template to be made recursive
-   * @param processors Processors
-   */
-  function addRecursiveProperty(endWhen: RecursionIterator, tmpl: T) {
-    const iterationState = iterateNext(endWhen, tmpl);
+    const tmpl = node.parent.value;
+    const clonedTmpl = clone(tmpl);
+    const buildableTmpl = asBuildable(clonedTmpl);
+    const iteration = iterateNext(endWhen, buildableTmpl.value);
 
-    const builder = createBuilderFn(() => {
-      let result = clone(tmpl);
-
-      if (iterationState.continue === true) {
-        (result as any)[property] = addRecursiveProperty(endWhen, tmpl);
-      } else {
-        (result as any)[property] = iterationState.endWithValue;
-      }
-
-      return result;
-    });
-
-    return createBuildable(builder, processors);
+    if (iteration.continue === true) {
+      node.value = buildChild(buildableTmpl, node);
+    } else {
+      node.value = iteration.endWithValue;
+    }
   }
 
   /**
@@ -56,7 +49,7 @@ export function withRecursion<T>(
    * and with value the recursion should stop. Recursion instructor functions avoid inifinite loops.
    * @param tmpl The template to pass in to the instructor function.
    */
-  function iterateNext(iteratorFn: RecursionIterator, tmpl: T): IterationState {
+  function iterateNext(iteratorFn: RecursionIterator, tmpl: any): IterationState {
     const value = iteratorFn(tmpl);
     const state = typeof value === 'function' ? value(tmpl) : value;
 
