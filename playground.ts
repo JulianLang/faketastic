8881137b6d52bd3d82cd7274605777969a779b97
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-08 20:46:33 +0100

    refactor: rename example into playground

--- playground.ts
+++ playground.ts
@@ -5,7 +5,7 @@
   To make git ignore this file for changes, enter this command into a terminal:
 
     $ cd path/to/this/repository/root
-    $ git update-index --assume-unchanged example.ts
+    $ git update-index --assume-unchanged playground.ts
 
   Now you can edit this file without worrying about checking in these changes.
   See also: https://stackoverflow.com/a/17410119/3063191 or https://git-scm.com/docs/git-update-index
@@ -36,16 +36,17 @@ const Directory = template({
   directories: recursion(
     'directories',
     RecursionDepth(2, 3),
-    quantity(() => randomInt(2, 3)),
+    quantity(() => randomInt(1, 2)),
   ),
 });
 
+// @ts-ignore
 const ExtD = extend(Directory, {
   parent: ref('name'),
 });
 
 const output = build(ExtD);
 
-// console.log(JSON.stringify(output, null, 2));
-console.log(output);
+console.log(JSON.stringify(output, null, 2));
+// console.log(output);
 console.log();
