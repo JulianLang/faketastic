2656738532528e69995ff95b349971b3bee81c7a
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-10 19:21:47 +0100

    feat: add helper is unset

--- src/util/index.ts
+++ src/util/index.ts
@@ -9,5 +9,6 @@ export * from './is-array';
 export * from './is-date.instance';
 export * from './is-defined';
 export * from './is-undefined';
+export * from './is-unset';
 export * from './is-valid.date';
 export * from './set-symbol';
