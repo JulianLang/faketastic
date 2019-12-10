d606c9ac487a485a28d4f03f34b7a3472cb092c6
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-10 12:58:00 +0100

    chore: remove unused code

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -1,6 +1,5 @@
 export * from './build.cycle';
 export * from './buildable';
 export * from './builder.fn';
-export * from './dictionary';
 export * from './pure.object';
 export * from './symbols';
