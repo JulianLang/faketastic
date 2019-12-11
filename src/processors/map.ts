b72cfec53efaa95b2e7da3c823c8518ee2b3b0ed
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-11 11:36:14 +0100

    feat: always map bare value

--- src/processors/map.ts
+++ src/processors/map.ts
@@ -1,5 +1,6 @@
 import { ObjectTreeNode } from 'treelike';
 import { MutatingFnOrders } from '../constants/mutating-function.orders';
+import { unwrapIfBuildable } from '../core';
 import { ProcessorFn } from './types';
 import { createProcessorFn } from './util';
 
@@ -7,7 +8,8 @@ export function map<T = any, K = any>(mapFn: (param: T, node?: ObjectTreeNode) =
   return createProcessorFn(mapImpl, 'finalizer', MutatingFnOrders.processors.map);
 
   function mapImpl(node: ObjectTreeNode) {
-    const mapped = mapFn(node.value, node);
+    const bareValue = unwrapIfBuildable(node.value);
+    const mapped = mapFn(bareValue, node);
     node.value = mapped;
   }
 }
