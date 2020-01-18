5a4bdfbc53202ad2bfa239c4ce09c79d30dc346a
Julian Lang <julian.lang@hotmail.de>
2020-01-18 10:46:05 +0100

    chore: remove empty files

--- src/core/generators/random-date.generator.ts
+++
@@ -1,9 +0,0 @@
-// see: https://stackoverflow.com/a/31379050/3063191
-export function randomDate(start: Date, end: Date): Date {
-  const startTime = start.getTime();
-  const endTime = end.getTime();
-  const time = startTime + Math.random() * (endTime - startTime);
-  const date = new Date(time);
-
-  return date;
-}
