ab89239ac8b78cf7188d03882c0966cbd8851ed8
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-01-26 23:18:03 +0100

    fix: typing

--- src/builder/builder.fn.ts
+++ src/builder/builder.fn.ts
@@ -2,4 +2,4 @@ import { AttachedFn } from '../attached-fns';
 import { Func } from '../types';
 
 /** Builds a given input into another output format. */
-export type BuilderFn<T = any> = Func<[any, AttachedFn[]?], T>;
+export type BuilderFn<T = any> = Func<[any, ...AttachedFn[]], T>;
