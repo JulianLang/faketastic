54b4d287ea639cf0589e0d0fa71b54b622f61e0b
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-01-26 18:19:14 +0100

    feat: add isProcessorFn type checker

---
+++ src/attached-fns/is-processor.fn.ts
@@ -0,0 +1,19 @@
+import { TimeOfExecution, Types } from '../constants';
+import { AnyFn } from '../types';
+import { getSymbol, isDefined, isType } from '../util';
+import { ExecutionTime } from './execution.time';
+
+/**
+ * Determines whether a given value is a `ProcessorFn`. Can optionally check for the time of execution as well.
+ * @param fn The function to check if it is a `ProcessorFn`.
+ * @param withTimeOfExecution (Optional) When provided, it checks if the time of execution matches the given value.
+ */
+export function isProcessorFn(fn: AnyFn, withTimeOfExecution?: ExecutionTime): boolean {
+  if (!isType(fn, Types.ProcessorFn)) {
+    return false;
+  }
+
+  return isDefined(withTimeOfExecution)
+    ? getSymbol(TimeOfExecution, fn) === withTimeOfExecution
+    : true;
+}
