624ed0713d50087d920c326d362e870f43edd382
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-02 17:35:11 +0100

    refactor: add attachedFns parameter to asBuildable

--- src/buildable/as-buildable.ts
+++ src/buildable/as-buildable.ts
@@ -1,3 +1,4 @@
+import { AttachedFn } from '../attached-fns';
 import { Buildable } from './buildable';
 import { createBuildable } from './create-buildable';
 import { isBuildable } from './is-buildable';
@@ -5,8 +6,16 @@ import { isBuildable } from './is-buildable';
 /**
  * Converts a given value into a `Buildable`, if it is not already one.
  * @param value The value to be converted into a `Buildable`, if necessary.
+ * @param attachedFns An array of attached functions that should be added to the `Buildable`.
  * @returns a newly created `Buildable` if the value wan none, the original instance otherwise.
+ * Optionally containing specified attached functions.
  */
-export function asBuildable(value: any): Buildable {
-  return isBuildable(value) ? value : createBuildable(value);
+export function asBuildable(value: any, attachedFns: AttachedFn[] = []): Buildable {
+  const target = isBuildable(value) ? value : createBuildable(value);
+
+  if (attachedFns.length > 0) {
+    target.attachedFns.push(...attachedFns);
+  }
+
+  return target;
 }
