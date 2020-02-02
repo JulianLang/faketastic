c815158fc1cdd1e16c2ef34409fd25fb313eed5c
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-02 19:13:49 +0100

    fix: typings of type checker functions

--- src/attached-fns/util/is-attached.fn.ts
+++ src/attached-fns/util/is-attached.fn.ts
@@ -1,4 +1,4 @@
-import { AnyFn } from '../../types';
+import { ArchitectFn, ProcessorFn, ReaderFn } from '../attached.fn';
 import { isArchitectFn } from './is-architect.fn';
 import { isProcessorFn } from './is-processor.fn';
 import { isReaderFn } from './is-reader.fn';
@@ -8,6 +8,6 @@ import { isReaderFn } from './is-reader.fn';
  * @param fn The function to be checked if it is an `AttachedFn`.
  * @returns `true` if the given value is an `AttachedFn`, `false` otherwise.
  */
-export function isAttachedFn(fn: AnyFn): boolean {
+export function isAttachedFn(fn: any): fn is ProcessorFn | ArchitectFn | ReaderFn {
   return isProcessorFn(fn) || isArchitectFn(fn) || isReaderFn(fn);
 }
--- src/attached-fns/util/is-reader.fn.ts
+++ src/attached-fns/util/is-reader.fn.ts
@@ -1,12 +1,12 @@
 import { Types } from '../../constants';
-import { AnyFn } from '../../types';
 import { isType } from '../../util';
+import { ReaderFn } from '../attached.fn';
 
 /**
  * Determines whether a given value is a `ReaderFn`.
  * @param fn The function to check if it is a `ReaderFn`.
  * @returns `true` if the given value is a `ReaderFn`, `false` otherwise.
  */
-export function isReaderFn(fn: AnyFn): boolean {
+export function isReaderFn(fn: any): fn is ReaderFn {
   return isType(fn, Types.ReaderFn);
 }
