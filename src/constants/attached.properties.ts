feb16db86b568be408b1648296b80932180fb170
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-08 21:20:54 +0100

    docs: add comments for AP

--- src/constants/attached.properties.ts
+++ src/constants/attached.properties.ts
@@ -1,7 +1,21 @@
 /** The indentifiers for attached properties set by faketastic's attached functions. */
 export default {
+  /** List of attached properties for faketastic's `range`-property function. */
   range: {
-    /** Set by attached range's strategy() attached-function. */
+    /**
+     * Set this attached property to override range's strategy to generate numbers.
+     *
+     * ---
+     *
+     * Example:
+     *
+     * ```ts
+     * const customRangeGen = (min, max) => 42;
+     * const myModel = model({
+     *   property: range(1, 10, attach(AP.range.strategy, customRangeGen)),
+     * });
+     * ```
+     */
     strategy: 'faketastic:range.strategy',
   },
 };
