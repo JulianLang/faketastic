f084222218d839bec597387ddcff5c9d31be84cd
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-09 18:36:00 +0100

    docs: add docs for oneOf

--- src/property-fns/one-of.ts
+++ src/property-fns/one-of.ts
@@ -3,6 +3,11 @@ import { createBuildable } from '../buildable';
 import { cloneItems } from '../util';
 import { createValueFn, randomItem } from '../value-fns';
 
+/**
+ * Chooses a random item from the given array of items.
+ * @param values The values to randomly choose an item from.
+ * @param attachedFns An arbitrary number of attached functions to manipulate the property's value.
+ */
 export function oneOf(values: any[], ...attachedFns: AttachedFn[]) {
   function oneOfImpl() {
     const cloned = cloneItems(values);
