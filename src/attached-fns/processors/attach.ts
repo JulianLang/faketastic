1c8ebefb0541069c99a2ebb9f9167d776baf494b
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-09 14:25:42 +0100

    docs: add comment for attach

--- src/attached-fns/processors/attach.ts
+++ src/attached-fns/processors/attach.ts
@@ -1,6 +1,11 @@
 import { Buildable } from '../../buildable';
 import { createProcessorFn } from '../util/create-processor.fn';
 
+/**
+ * Sets the specified attached property with the given value on the current property's buildable.
+ * @param property The attached property identifier to set a value on.
+ * @param value The value to be set.
+ */
 export function attach(property: string, value: any) {
   function attachProperty(buildable: Buildable) {
     buildable.attachedProperties[property] = value;
