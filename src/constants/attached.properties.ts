7d41a309f0ef91ce7493451fcf74e6fd9297df47
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-09 19:57:00 +0100

    feat: add randomItems attached property identifier and docs

--- src/constants/attached.properties.ts
+++ src/constants/attached.properties.ts
@@ -1,7 +1,14 @@
 /** The indentifiers for attached properties set by faketastic's attached functions. */
 export default {
+  /**
+   * Provides the attached property identifiers used to retrieve default strategy implementations.
+   * Use these attached properties to locally or globally replace them with your custom implementations.
+   */
   strategies: {
+    /** Attached property identifier for the probability implementation. */
     probability: 'faketastic:strategies.probability',
+    /** Attached property identifier for the randomItems implementation. */
+    randomItems: 'faketastic:strategies.randomItems',
   },
   /** List of attached properties for faketastic's `range`-property function. */
   range: {
