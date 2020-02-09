2dbe67253d065debdf476dfc2ae9e863c241dabf
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-09 12:43:46 +0100

    chore: remove comment

--- src/attached-fns/processors/attach.ts
+++ src/attached-fns/processors/attach.ts
@@ -2,7 +2,6 @@ import { Buildable } from '../../buildable';
 import { createProcessorFn } from '../util/create-processor.fn';
 
 export function attach(property: string, value: any) {
-  // @Processor('prebuild', { read: 'buildable' })
   function attachProperty(buildable: Buildable) {
     buildable.attachedProperties[property] = value;
 
