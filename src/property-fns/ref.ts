db115ffe9f0211ceccc10a50c184304b18b6730d
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-12 22:12:15 +0100

    refactor: simplify ref

--- src/property-fns/ref.ts
+++ src/property-fns/ref.ts
@@ -1,10 +1,10 @@
-import { findNode, ObjectTreeNode, parentTraverser } from 'treelike';
+import { findNode, isDefined, ObjectTreeNode, parentTraverser } from 'treelike';
 import { AttachedFn, createReaderFn } from '../attached-fns';
 import { Buildable, createBuildable } from '../buildable';
 import { getRawValue } from '../builder';
 import { Types } from '../constants';
 import AP from '../constants/attached.properties';
-import { isUndefined, setType } from '../util';
+import { setType } from '../util';
 import { createValueFn } from '../value-fns';
 
 export function ref(property: string, ...attachedFns: AttachedFn[]) {
@@ -17,15 +17,11 @@ export function ref(property: string, ...attachedFns: AttachedFn[]) {
       parentTraverser,
     );
 
-    if (isUndefined(result)) {
-      console.warn(`faketastic: Could not resolve reference to "${property}"`);
-      // TODO: langju: set result = UnsetValue or Placeholder...
+    if (isDefined(result)) {
+      const buildable: Buildable = node.value;
+      const referenceFn = setType(Types.ReferenceFn, () => getRawValue(result?.value));
+      buildable.attachedProperties[AP.ref.resolvedValue] = referenceFn;
     }
-
-    const buildable: Buildable = node.value;
-
-    const referenceFn = setType(Types.ReferenceFn, () => getRawValue(result?.value));
-    buildable.attachedProperties[AP.ref.resolvedValue] = referenceFn;
   }
 
   function refImpl(buildable: Buildable) {
