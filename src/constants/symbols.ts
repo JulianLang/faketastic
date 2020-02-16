34edd3e4d465fab8e67fd9b6347080f20759d2f2
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2020-02-16 13:00:25 +0100

    lint: fix: warnings

--- src/constants/symbols.ts
+++ src/constants/symbols.ts
@@ -1,8 +1,12 @@
 /** Holds meta information about what value to pass into an mutation function (architects and processors). */
+// tslint:disable-next-line: variable-name
 export const MutationFnReadType = Symbol('faketastic.symbols.attached-fn-read-type');
 /** Holds meta information about an object's or function's type. */
+// tslint:disable-next-line: variable-name
 export const Type = Symbol('faketastic.symbols.type');
 /** Holds meta information about the time of execution of a function (mostly `ProcessorFn`s). */
+// tslint:disable-next-line: variable-name
 export const TimeOfExecution = Symbol('faketastic.symbols.time-of-execution');
 /** Holds meta information about whether a faketastic-attached-function was already called. */
+// tslint:disable-next-line: variable-name
 export const FnCalled = Symbol('faketastic.symbols.fn-called');
